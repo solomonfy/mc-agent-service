a13d8d5da3ff323c111f494340eb2f9bccd052cf
solomonfy <fantahunsolomon@gmail.com>
2022-03-23 01:08:10 -0400

    set main branch as default branch

--- src/main/java/com/medochemie/ordermanagement/agentservice/repository/AgentRepositoryCustom.java
+++ src/main/java/com/medochemie/ordermanagement/agentservice/repository/AgentRepositoryCustom.java
@@ -3,5 +3,6 @@ package com.medochemie.ordermanagement.agentservice.repository;
 import com.medochemie.ordermanagement.agentservice.entity.Agent;
 
 public interface AgentRepositoryCustom {
+    
     long updateAgent(Agent agent);
 }
